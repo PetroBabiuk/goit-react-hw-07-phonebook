d64f98933f915e12868e92982199882b80e23e34
Petro Babiuk <75635325+PetroBabiuk@users.noreply.github.com>
2021-10-01 11:35:20 +0300

    delete code responsible for LS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import {store, persistor} from './redux/store';
+import {store} from './redux/store';
 import App from './App';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
@@ -11,9 +10,7 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <App />
-      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
