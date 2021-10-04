61b6800f1df7bc005174c1c4412b20eff4828d04
Petro Babiuk <75635325+PetroBabiuk@users.noreply.github.com>
2021-10-04 12:28:18 +0300

    add createAsyncThunk

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,30 +2,30 @@ import { createAction } from '@reduxjs/toolkit';
 
 ////////////////////////     FETCH CONTACTS     //////////////////////////////////////
 
-// pending
-export const fetchContactsRequest = createAction('contacts/fetchContactsRequest');
-// fulfilled
-export const fetchContactsSuccess = createAction('contacts/fetchContactsSuccess');
-// rejected
-export const fetchContactsError = createAction('contacts/fetchContactsError');
+// // pending
+// export const fetchContactsRequest = createAction('contacts/fetchContactsRequest');
+// // fulfilled
+// export const fetchContactsSuccess = createAction('contacts/fetchContactsSuccess');
+// // rejected
+// export const fetchContactsError = createAction('contacts/fetchContactsError');
 
 ////////////////////////     DELETE CONTACT    //////////////////////////////////////
 
-// pending
-export const deleteContactRequest = createAction('phoneBook/deleteContactRequest');
-// fulfilled
-export const deleteContactSuccess = createAction('phoneBook/deleteContactSuccess');
-// rejected
-export const deleteContactError = createAction('phoneBook/deleteContactError');
+// // pending
+// export const deleteContactRequest = createAction('phoneBook/deleteContactRequest');
+// // fulfilled
+// export const deleteContactSuccess = createAction('phoneBook/deleteContactSuccess');
+// // rejected
+// export const deleteContactError = createAction('phoneBook/deleteContactError');
 
 ////////////////////////     ADD CONTACT //////////////////////////////////////
 
-// pending
-export const addContactRequest = createAction('phoneBook/addContactRequest');
-// fulfilled
-export const addContactSuccess = createAction('phoneBook/addContactSuccess');
-// rejected
-export const addContactError = createAction('phoneBook/addContactError');
+// // pending
+// export const addContactRequest = createAction('phoneBook/addContactRequest');
+// // fulfilled
+// export const addContactSuccess = createAction('phoneBook/addContactSuccess');
+// // rejected
+// export const addContactError = createAction('phoneBook/addContactError');
 
 ////////////////////////     CHANGE FILTER    //////////////////////////////////////
 
